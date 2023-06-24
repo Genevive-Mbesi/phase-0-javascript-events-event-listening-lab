6a250b90ff47c267f59ed5c8f835b2ff502f825b
Genevive-Mbesi <genevive.opondo@student.moringaschool.com>
2023-06-24 15:48:07 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
+    alert('binds an event listener in addingEventListener()')
 }
+
+input.addEventListener('true', addingEventListener);
